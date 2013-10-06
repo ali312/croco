ceddb3d9673e40884b864913748621da7e4f272f
ali312 <ali312@yandex.ru>
2013-10-06 20:43:37 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Croco::Application.config.secret_token = '5226b0b3f91765cc019dd7f1ef44550b95917f6302bec6864970b307b686ef393410ebad7e698b3fafb32a4fadb194bdba845ab2f500b16e875090853d761566'
